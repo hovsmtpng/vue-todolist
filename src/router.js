6afd37ec68fd1585bfbf9cd0140ba2a5058c47ba
hovsmtpng <siburiansimatupang98@gmail.com>
2022-02-08 22:40:55 +0700

    router and dependencies update

--- src/router.js
+++ src/router.js
@@ -2,11 +2,6 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
-// lazy-loaded
-const Profile = () => import("./components/Profile.vue")
-const BoardAdmin = () => import("./components/BoardAdmin.vue")
-const BoardModerator = () => import("./components/BoardModerator.vue")
-const BoardUser = () => import("./components/BoardUser.vue")
 
 const routes = [
   {
@@ -25,30 +20,6 @@ const routes = [
   {
     path: "/register",
     component: Register,
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    // lazy-loaded
-    component: Profile,
-  },
-  {
-    path: "/admin",
-    name: "admin",
-    // lazy-loaded
-    component: BoardAdmin,
-  },
-  {
-    path: "/mod",
-    name: "moderator",
-    // lazy-loaded
-    component: BoardModerator,
-  },
-  {
-    path: "/user",
-    name: "user",
-    // lazy-loaded
-    component: BoardUser,
   },
 ];
 
